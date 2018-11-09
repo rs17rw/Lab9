2090b6b6e94ff209100ea5c1dff1479efb43f895
rs17rw <43220367+rs17rw@users.noreply.github.com>
2018-11-08 21:51:53 -0500

    Add files via upload

---
+++ Lab9Exercise1.js
@@ -0,0 +1,6 @@
+/A Function to printout Hurrah! when called
+
+function pushMe() {
+  var text1= Hurrah;
+	document.getElementById("output").innerHTML="text1";
+}
---
+++ Lab9Exercise3.js
@@ -0,0 +1,7 @@
+//This will output when the page loads
+document.write("Output Zero Here");
+
+//This will output when a button is pressed
+function therest(){
+  var output3= output3;
+	document.ElementById("output3").innerHTML="Output Three Here";
